faca694c5e1de1491f55c13f146512e851677887
Stefano Esposito <stefano.esposito@abstract.it>
2024-08-08 14:34:17 +0200

    accordion with intro

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import ScrollToTopButton from "./components/ScrollToTopButton";
 import { Input, Button } from "@mui/material";
 import { BiRefresh } from "react-icons/bi";
 import Footer from "./components/Footer";
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function App() {
     const [sutras, setSutras] = useState([]);
@@ -162,6 +167,54 @@ function App() {
                         </nav>
                     )}
 
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ArrowDropDownIcon />}
+                            aria-controls="panel2-content"
+                            id="panel2-header"
+                        >
+                            <Typography>Leggi l'introduzione</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                <p>Il <em>Vivekacūḍāmaṇi</em> (letteralmente il "Gran Gioiello della Discriminazione") è un'opera letteraria attribuita al grande Maestro Samkara, uno dei principali esponenti dell'Advaita Vedanta, la Scuola della non-dualità. Si tratta di uno dei più popolari manuali di insegnamento, uno dei testi di "sostentamento spirituale" della tradizione Advaita Vedanta. Non si tratta di un testo semplicemente "filosofico o polemico": è principalmente una <strong>pratica istruzione</strong>, un aiuto concreto al viaggio spirituale di un Advaitin verso la liberazione (<em>Moksha</em>), non quindi un testo di "filosofia per amore della filosofia".</p>
+
+                                <p>Il testo consiste di 580 versi (<em>sūtra</em>) ed è narrato sotto forma di <em>satsang</em> (dialogo tra maestro e discepolo) in cui si susseguono tematiche relative a religione, spiritualità, metafisica, filosofia, con lo scopo di aiutare l'individuo a liberarsi dall'illusione attraverso l'uso della discriminazione (<em>Viveka</em>).</p>
+
+                                <p>Per Samkara è esclusivamente questa <em>Viveka</em>, il discernimento, e conseguentemente il <em>Vairagya</em> (il distacco) a portare all'integrale soluzione dei coaguli energetici che velano l'identità col <em>Brahman</em> e quindi alla Libeazione (Moksha).</p>
+
+                                <p>Tutte le altre pratiche religiose come la <em>bhakti</em> (la devozione nei confronti della divinità), o l'attività sacrificale e le restanti pratiche cultuali e morali, non consentono il raggiungimento del fine ultimo, ma solo l'ottenimento di rinascite favorevoli.</p>
+
+                                <p>Nel trattato, celebrato per secoli come un lucido trattato introduttivo all'Advaita Vedanta, i concetti sono presentati in modo penetrante, incisivo e chiaro, e comprendono una serie di soggetti che vanno dalla preparazione di sé, all'imparare a discernere il reale dal non-reale, al controllare la propria personalità al fine di liberarsi, alla finale realizzazione dell'Essere-senza-secondo.</p>
+
+                                <p>Nel Dialogo vengono inotre dettagliatamente descritte i cinque "involucri" (detti <em>Kosha</em>) o "guaine" che velano la conoscenza dell'Ātman: </p>
+
+                                <ol>
+                                    <li>l'involucro fatto di cibo (corpo grossolano)</li>
+                                    <li>l'involucro fatto di energia vitale (parte del corpo sottile)</li>
+                                    <li>l'involucro fatto di mente (parte del corpo sottile)</li>
+                                    <li>l'involucro fatto di intelletto (parte del corpo sottile)</li>
+                                    <li>l'involucro fatto di beatitudine (<em>ananda</em>) (corpo causale o spirituale)</li>
+                                </ol>
+
+                                <p>Mentre il dalogo si snoda, la coscienza del Discepolo è riaccordata, ed "intonata" sulla nota fondamentale dell'Essere-Brahman, fino a trasfigurarsi completamente e realizzare la propria identità senza-secondo.</p>
+
+                                <p>Al sutra 483 il Discepolo esclama:</p>
+                                <p><em>"Dove se n’è andato l’universo? Chi l’ha fatto svanire? L’ho appena scorto ed ecco che esso è già sparito. O meraviglia di un miraggio!"</em></p>
+                                <p>e più avanti nel testo (sutra 496) realizza:</p>
+                                <p><em>"Sono l’oceano dell’illimitata beatitudine ed è in me che le onde senza fine dell’universo si formano e si dissolvono nel gioco capriccioso della māyā."</em></p>
+
+                                <p>Alla fine di questo dialogo-satsang il discepolo, realizza la propria identità di <em>Ātman-Brahman</em> (<em>"In verità sono questo Brahman non-duale, sostrato di tutti i fenomeni, che illumina con la sua luce tutto lo spettacolo, che assume molteplici forme, che è onnipresente, eterno, puro, immutabile e assoluto"</em>); quindi si congeda dal Maestro, e le loro strade si dividono, senza attaccamento, senza alcun residuo di sentimentalismo.</p>
+                                <p>In realtà non c'è più un "discepolo": realizzato l'Essere-senza-secondo, un nuovo Maestro va per la sua strada, e si offrirà spontaneamente a chi busserà per essere.</p>
+
+
+
+                                <div>
+                                    <p><small>Il testo è tratto da <strong><em>Vivekacūḍāmaṇi</em>: Il gran gioiello della discriminazione</strong> (a cura di Raphael), Editrice Asram Vidya, Roma, 1981</small></p>
+                                </div>
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
                     {sutras
                         .filter((sutra) => {
                             const normalizedSutra = sutra.sutra
